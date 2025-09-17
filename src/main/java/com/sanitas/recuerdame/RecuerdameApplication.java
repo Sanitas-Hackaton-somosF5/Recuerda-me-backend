83d0f35a198db001c2ce770aca8ba5368b26965e
Lara Pla <laraplamoreno@gmail.com>
2025-09-17 11:35:40 +0200

    build(src): build project

---
+++ src/main/java/com/sanitas/recuerdame/RecuerdameApplication.java
@@ -0,0 +1,13 @@
+package com.sanitas.recuerdame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuerdameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuerdameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanitas/recuerdame/RecuerdameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanitas.recuerdame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuerdameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
