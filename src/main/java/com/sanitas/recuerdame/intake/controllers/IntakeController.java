7f0edacc424b4fbcc8fc9d4ea8795dc904630d8f
Jesus Martin <jesus8.mb@gmail.com>
2025-09-18 11:41:48 +0200

    fix: IntakeController medicationId fixed naming

--- src/main/java/com/sanitas/recuerdame/intake/controllers/IntakeController.java
+++ src/main/java/com/sanitas/recuerdame/intake/controllers/IntakeController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +24,7 @@ public class IntakeController {
     this.service = service;
   }
 
-  @GetMapping("")
+  @GetMapping()
   public List<IntakeResponse> index() {
     return service.getAllIntakes();
   }
@@ -37,19 +35,14 @@ public class IntakeController {
     return ResponseEntity.ok(intake);
   }
 
-  @PostMapping("")
-  public ResponseEntity<IntakeResponse> storeIntake(@RequestBody IntakeRequest dtoRequest) {
-    if (dtoRequest == null) {
-      return ResponseEntity.badRequest().build();
-    }
+  @GetMapping("/today")
+  public List<IntakeResponse> todayIntake() {
+    return service.getTodayIntakes();
+  }
 
-    IntakeResponse entityStored = service.createIntake(dtoRequest);
-
-    if (entityStored == null) {
-      return ResponseEntity.noContent().build();
-    }
-
-    return ResponseEntity.status(201).body(entityStored);
+  @GetMapping("/medication/{medicationId}")
+  public List<IntakeResponse> getIntakeByMedication(@PathVariable("medicationId") Long medicationId) {
+    return service.getIntakesByMedication(medicationId);
   }
 
   @PatchMapping("/{id}/{status}")
