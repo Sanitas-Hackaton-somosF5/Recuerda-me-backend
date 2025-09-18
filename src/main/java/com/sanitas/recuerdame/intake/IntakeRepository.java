60e921ddd6ef8152e2a0ed75783e8167ee84c63a
Jesus Martin <jesus8.mb@gmail.com>
2025-09-18 11:06:10 +0200

    fix Repository

--- src/main/java/com/sanitas/recuerdame/intake/IntakeRepository.java
+++ src/main/java/com/sanitas/recuerdame/intake/IntakeRepository.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IntakeRepository extends JpaRepository<Intake, Long> {
   List<Intake> findByDate(LocalDate date);
 
-  List<Intake> findByMedication(Long medicationId);
+  @Query("SELECT i FROM Intake i WHERE i.medication.id = :medicationId")
+  List<Intake> findByMedication(@Param("medicationId") Long medicationId);
 }
--- src/main/java/com/sanitas/recuerdame/intake/controllers/IntakeController.java
+++ src/main/java/com/sanitas/recuerdame/intake/controllers/IntakeController.java
@@ -3,12 +3,9 @@ package com.sanitas.recuerdame.intake.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,8 +34,6 @@ public class IntakeController {
     IntakeResponse intake = service.getIntakeById(id);
     return ResponseEntity.ok(intake);
   }
-
-  // TODO: Update and date path
 
   @GetMapping("/today")
   public List<IntakeResponse> todayIntake() {
